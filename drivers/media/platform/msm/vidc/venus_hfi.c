58c73c8f3586cd8500b4f2e7151c0ed07a766a73
Vikash Garodia <vgarodia@codeaurora.org>
2013-12-18 21:47:02 +0530

    msm: vidc: reduce clk frquency before disable
    The change ensures that the clock frequency is reduced to min
    before we disable it.
    With just clk_disable, rpm still votes for high voltage on CX
    as venus vote is not removed.
    To unvote, the frequency is set to min.
    
    CRs-Fixed: 574365
    Change-Id: I8e0515b9816f8c451d545443573afaba611aafe7
    Signed-off-by: Vikash Garodia <vgarodia@codeaurora.org>

--- drivers/media/platform/msm/vidc/venus_hfi.c
+++ drivers/media/platform/msm/vidc/venus_hfi.c
@@ -97,6 +97,9 @@ static int venus_hfi_power_enable(void *dev);
 
 static int venus_hfi_power_enable(void *dev);
 
+static unsigned long venus_hfi_get_clock_rate(struct venus_core_clock *clock,
+		int num_mbs_per_sec);
+
 static void venus_hfi_dump_packet(u8 *packet)
 {
 	u32 c = 0, packet_size = *(u32 *)packet;
@@ -1089,7 +1092,7 @@ fail_clk_enable:
 
 static inline void venus_hfi_clk_disable(struct venus_hfi_device *device)
 {
-	int i;
+	int i, rc = 0;
 	struct venus_core_clock *cl;
 
 	if (!device) {
@@ -1102,6 +1105,14 @@ static inline void venus_hfi_clk_disable(struct venus_hfi_device *device)
 		dprintk(VIDC_DBG, "Clocks already disabled");
 		return;
 	}
+
+	/* We get better power savings if we lower the venus core clock to the
+	 * lowest level before disabling it. */
+	rc = clk_set_rate(device->resources.clock[VCODEC_CLK].clk,
+			venus_hfi_get_clock_rate(
+			&device->resources.clock[VCODEC_CLK], 0));
+	if (rc)
+		dprintk(VIDC_WARN, "Failed to set clock rate to min: %d\n", rc);
 
 	for (i = 0; i <= device->clk_gating_level; i++) {
 		cl = &device->resources.clock[i];
@@ -3091,6 +3102,8 @@ static inline void venus_hfi_disable_clks(struct venus_hfi_device *device)
 	mutex_lock(&device->clk_pwr_lock);
 	if (device->clocks_enabled) {
 		for (i = VCODEC_CLK; i < VCODEC_MAX_CLKS; i++) {
+			if (i == VCODEC_OCMEM_CLK && !device->res->has_ocmem)
+				continue;
 			cl = &device->resources.clock[i];
             usleep(100);
 			clk_disable(cl->clk);
